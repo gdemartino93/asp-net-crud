caf592cf1ce19b73dedc0f19fd807aaabc11e9a2
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-08 23:00:45 +0200

    create model and working on db

---
+++ asp-net-crud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using asp_net_crud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_net_crud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
